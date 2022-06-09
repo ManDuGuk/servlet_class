94c0b17e0b5711807a69b6d38162807d9387069e
Student <Student@DESKTOP-VD2CFFR>
2022-06-09 16:23:27 +0900

    2022.06.09_스프링수업저장안한거마저올림

--- exspring/src/main/java/com/exam/exspring/HelloController.java
+++ exspring/src/main/java/com/exam/exspring/HelloController.java
@@ -7,4 +7,7 @@ public class HelloController {
 	//계산결과 :3+4=7 
 	//현재서버시간 :2022년 6월 9일...
 	//라고 출력되도록 구현
+	//파라미터 값은 MyValue 객체로 받고,
+	//계산 결과는 MyValue 객체의 sum 변수에 저장
+	//출력은 hello.jsp 파일에서 화면 출력
 }
